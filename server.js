907dbbe33757d2f78fa3a3c7ac6d746c1415c040
Jacob Nollen <jacob.nollen@netplanner.com>
2023-03-19 22:50:05 -0400

    changed listen port to PORT from APP_PORT

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = process.env.APP_PORT;
+const port = process.env.PORT;
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
